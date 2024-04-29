df7347bcbe0b58b89e3208c405f878365b394aef
Kritchanaxt <kritchanatweve@gmail.com>
2024-04-30 01:47:43 +0700

    Initial Commit

---
+++ SwiftUI-DesignPatterns/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-DesignPatterns
+//
+//  Created by Kritchanaxt_. on 30/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-DesignPatterns/SwiftUI_DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignPatternsApp.swift
+//  SwiftUI-DesignPatterns
+//
+//  Created by Kritchanaxt_. on 30/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
